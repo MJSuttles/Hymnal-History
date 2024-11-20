0ef62ae2d39fd2355cd7894fd10d4d3a2d2b5ce9
MJSuttles <rolltiderolldad@gmail.com>
2024-11-19 21:05:42 -0600

    fixed navbar to state mvp requirements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,20 +8,26 @@ export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Link passHref href="/" className='navbar-brand'>
-         CHANGE ME
+        <Link passHref href="/">
+          <Navbar.Brand>
+            <img src="/../images/hymnal-hero.png" alt="Hymnal Hero Logo" width={150} height={80} style={{ marginLeft: '10px' }} />
+          </Navbar.Brand>
         </Link>
+        Hymnal Hero
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link className="nav-link" href="/">
-              Home
+              Songs
+            </Link>
+            <Link className="nav-link" href="/songs/new">
+              Add a Song
             </Link>
           </Nav>
 
           <Button variant="danger" onClick={signOut}>
-              Sign Out
+            Sign Out
           </Button>
         </Navbar.Collapse>
       </Container>
